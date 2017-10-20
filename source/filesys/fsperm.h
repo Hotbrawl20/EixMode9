2acb71a7848e8e5ba805d3e34d5e5b2a4ed368f9
Eix <eixxhis@gmail.com>
2017-10-20 11:39:15 -0400

    spelling error

--- source/filesys/fsperm.h
+++ source/filesys/fsperm.h
@@ -17,7 +17,7 @@
 #define PERM_GAME       (1UL<<11) // can't be enabled, placeholder
 #define PERM_XORPAD     (1UL<<12) // can't be enabled, placeholder
 #define PERM_CART       (1UL<<13) // can't be enabled, placeholder
-#defime PERM_BASE       (PERM_ALL)
+#define PERM_BASE       (PERM_ALL)
 #define PERM_BASE2      (PERM_SDCARD | PERM_IMAGE | PERM_RAMDRIVE | PERM_EMU_LVL0 | PERM_SYS_LVL0)
 #ifndef SAFEMODE
 #define PERM_ALL        (PERM_BASE2 | PERM_SDDATA | PERM_EMU_LVL1 | PERM_SYS_LVL3 | PERM_MEMORY)
