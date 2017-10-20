4247f724c06d849b27003fabc89ca48a4bca1ebe
Eix <eixxhis@gmail.com>
2017-10-20 11:28:25 -0400

    fixed a minor fuck up
    was messing with my firm partitions and noticed a write lock, it should be fixed now

--- source/filesys/fsperm.h
+++ source/filesys/fsperm.h
@@ -19,9 +19,9 @@
 #define PERM_CART       (1UL<<13) // can't be enabled, placeholder
 #define PERM_BASE       (PERM_SDCARD | PERM_IMAGE | PERM_RAMDRIVE | PERM_EMU_LVL0 | PERM_SYS_LVL0)
 #ifndef SAFEMODE
-#define PERM_ALL        (PERM_BASE | PERM_SDDATA | PERM_EMU_LVL1 | PERM_SYS_LVL2 | PERM_MEMORY)
+#define PERM_ALL        (PERM_BASE | PERM_SDDATA | PERM_EMU_LVL1 | PERM_SYS_LVL3 | PERM_MEMORY)
 #else
-#define PERM_ALL        (PERM_BASE | PERM_SDDATA | PERM_EMU_LVL1 | PERM_SYS_LVL1)
+#define PERM_ALL        (PERM_BASE | PERM_SDDATA | PERM_EMU_LVL1 | PERM_SYS_LVL3)
 #endif
 
 // permission levels / colors
