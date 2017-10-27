7f0f106eb51b006aca7abde4b3ac0a7639a3a5b6
Eix <eixxhis@gmail.com>
2017-10-27 09:51:20 -0400

    new colors
    fixed orange

--- source/common/ui.h
+++ source/common/ui.h
@@ -35,7 +35,9 @@
 #define COLOR_BLUE          RGB(0x00, 0x00, 0xFF)
 #define COLOR_YELLOW        RGB(0xFF, 0xFF, 0x00)
 #define COLOR_CYAN          RGB(0xFF, 0x00, 0xFF)
-#define COLOR_ORANGE        RGB(0x00, 0x00, 0xFF)
+#define COLOR_ORANGE        RGB(0xFF, 0x57, 0x33)
+#define COLOR_EIX           RGB(0x4C, 0x43, 0x51)//nice color, idk how it will show up on 3ds screen
+#define COLOR_PURPLE        RGB(ox4C, 0x43, 0x51)//^
 
 #define COLOR_BRIGHTRED     RGB(0xFF, 0x30, 0x30)
 #define COLOR_DARKRED       RGB(0x80, 0x00, 0x00)
